eacfcaf4c75c7d874c7aeb3d13a17246b8027fc0
Filip Lewiński <filip.lewinski@3mdeb.com>
2025-02-04 18:14:03 +0100

    cleanup
    Signed-off-by: Filip Lewiński <filip.lewinski@3mdeb.com>

--- src/include/config.hpp
+++ src/include/config.hpp
@@ -5,6 +5,10 @@
 const int WINDOW_WIDTH = 300;
 const int WINDOW_HEIGHT = 600;
 
+// World settings (game coordinate space)
+const int WORLD_WIDTH = 900; 
+const int WORLD_HEIGHT = 600;
+
 // Paddle settings
 const int PADDLE_WIDTH = 10;
 const int PADDLE_HEIGHT = 60;
@@ -16,12 +20,8 @@ const int PADDLE_INIT_Y = 250;
 const int BALL_SIZE = 10;
 const int BALL_SPEED_X = 3;
 const int BALL_SPEED_Y = 3;
-const int BALL_INIT_X = 300;
-const int BALL_INIT_Y = 300;
-
-// World settings (game coordinate space)
-const int WORLD_WIDTH = 900; 
-const int WORLD_HEIGHT = 600;
+const int BALL_INIT_X = WORLD_WIDTH/2;
+const int BALL_INIT_Y = WORLD_HEIGHT/2;
 
 #endif // CONFIG_HPP
 
